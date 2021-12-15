bf00c14bedd7bd91932e146541b5ec160bd9d4fd
Nick Heisler <42762547+nheisler@users.noreply.github.com>
2021-12-14 21:02:17 -0700

    Create UserVariables.py

---
+++ UserVariables.py
@@ -0,0 +1,13 @@
+#coin to focus on
+coin="LINK-USD"
+
+#how often an action should be taken (in seconds)
+duration=300
+
+#Minimum cash in account (in USD), where application should quit
+minimum_threshold=3
+
+#retrieve these from coinbase account
+key=""
+passphrase=""
+secret=""
